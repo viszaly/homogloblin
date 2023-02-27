6093ab6f0e2063d14ef97cbf1bf1cf4221748433
Viszály <71315111+viszaly@users.noreply.github.com>
2023-02-27 19:40:24 +0100

    Add files via upload

--- main2.js
+++ main2.js
@@ -12,3 +12,33 @@ function search() {
 function lucky() {
   window.location.href = "https://www.youtube.com/watch?v=xvFZjo5PgG0"
 }
+
+function darkTheme() {
+  document.querySelector('body').style.backgroundColor = "rgb(16, 16, 16)";
+  document.querySelector('html').style.backgroundColor = "rgb(16, 16, 16)";
+  document.querySelector('footer').style.backgroundColor = "rgb(19, 23, 26)";
+  document.getElementById("hu").style.color = "white";
+  document.getElementById("search-input").style.backgroundColor = "rgb(16, 16, 16)";
+  document.getElementById("search-input").style.color = "white";
+  document.getElementById("gs-btn").style.backgroundColor = "rgb(69, 69, 69)";
+  document.getElementById("ls-btn").style.backgroundColor = "rgb(69, 69, 69)";
+  document.getElementById("gs-btn").style.color = "white";
+  document.getElementById("ls-btn").style.color = "white";
+  document.getElementById("dark-btn").style.display = "none";
+  document.getElementById("light-btn").style.display = "block";
+}
+
+function lightTheme() {
+  document.querySelector('body').style.backgroundColor = "rgb(255, 255, 255)";
+  document.querySelector('html').style.backgroundColor = "rgb(255, 255, 255)";
+  document.querySelector('footer').style.backgroundColor = "rgb(242, 242, 242)";
+  document.getElementById("hu").style.color = "gray";
+  document.getElementById("search-input").style.backgroundColor = "rgb(255, 255, 255)";
+  document.getElementById("search-input").style.color = "black";
+  document.getElementById("gs-btn").style.backgroundColor = "rgb(248, 249, 250)";
+  document.getElementById("ls-btn").style.backgroundColor = "rgb(248, 249, 250)";
+  document.getElementById("gs-btn").style.color = "black";
+  document.getElementById("ls-btn").style.color = "black";
+  document.getElementById("dark-btn").style.display = "block";
+  document.getElementById("light-btn").style.display = "none";
+}
